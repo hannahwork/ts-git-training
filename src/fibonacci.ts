f65acfbc41d700c2df330795c54f8a0a036f1fef
Cameron McCormack <cameron.mccormack@softwire.com>
2022-06-19 15:20:43 +0100

    improve variable naming and use clearer computation

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -21,21 +21,21 @@ export const computeFibonacciNumber = (position: number | null, recursion: boole
 
     if (isRecursive) return recursiveFibonacciAlt(position);
 
-    let i = 1;
-    let j = 1;
-
-    if (notNullPosition <= 2) {
+    if (notNullPosition === 1 || position === 2) {
         return 1;
     }
 
+    let smallFibonacciNumber = 1;
+    let largeFibonacciNumber = 1;
+
     let currentPosition = 2;
     while (currentPosition < notNullPosition) {
-        const temp = i;
-        i = j;
-        j += temp;
+        const nextFibonacciNumber = smallFibonacciNumber + largeFibonacciNumber;
+        smallFibonacciNumber = largeFibonacciNumber;
+        largeFibonacciNumber = nextFibonacciNumber;
         currentPosition++;
     }
-    return j;
+    return largeFibonacciNumber;
 };
 
 const computeNegativeFibonacci = (position: number): number => {
